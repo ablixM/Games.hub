cb95d72fdbdc0fc9c0898f775e57251176a82376
Abraham Masresha <abrehammasresha890@gmail.com>
2024-10-04 18:00:50 +0300

    Improved ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
-import { Box, Flex, Grid, GridItem, Show } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Grid,
+  GridItem,
+  Show,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid.tsx";
 import GenreList from "./components/GenereList.tsx";
@@ -17,6 +24,7 @@ export interface GameQuery {
 
 function App() {
   const [gameQuery, setGameQuery] = useState<GameQuery>({} as GameQuery);
+  const bg = useColorModeValue("gray.50", "gray.800");
 
   return (
     <Grid
@@ -26,21 +34,38 @@ function App() {
       }}
       templateColumns={{ base: "1fr", lg: "300px 1fr" }}
     >
-      <GridItem area="nav">
+      <GridItem
+        area="nav"
+        position="fixed"
+        bg={bg}
+        left={0}
+        top={0}
+        right={0}
+        height={{ sm: "60px", md: "80px" }}
+        zIndex={9999}
+      >
         <NavBar
           onSearch={(searchText) => setGameQuery({ ...gameQuery, searchText })}
         />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
+        <GridItem
+          area="aside"
+          paddingX={5}
+          overflowY="auto"
+          maxHeight="700px"
+          position="fixed"
+          left={0}
+          marginTop={"80px"}
+        >
           <GenreList
             selectedGenre={gameQuery.genre}
             onSelectedGenre={(genre) => setGameQuery({ ...gameQuery, genre })}
           ></GenreList>
         </GridItem>
       </Show>
-      <GridItem area="main">
-        <Flex marginLeft={9} marginBottom={5}>
+      <GridItem area="main" marginTop={"80px"}>
+        <Flex marginLeft={9} marginBottom={15} zIndex={999}>
           <Box marginRight={5}>
             <PlatformSelector
               onSelectedPlatfrom={(platform) =>
@@ -56,7 +81,9 @@ function App() {
             }}
           />
         </Flex>
-        <GameGrid gameQuery={gameQuery} />
+        <Box marginTop={"20px"} zIndex={997}>
+          <GameGrid gameQuery={gameQuery} />
+        </Box>
       </GridItem>
     </Grid>
   );
